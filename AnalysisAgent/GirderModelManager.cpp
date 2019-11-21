ecbb708d4062f10b56e79fde781e2b2fd1c6b15c
Richard Brice <37087370+RickBrice@users.noreply.github.com>
2019-11-21 08:40:34 -0800

    Removes unused interface

--- AnalysisAgent/GirderModelManager.cpp
+++ AnalysisAgent/GirderModelManager.cpp
@@ -6631,7 +6631,6 @@ void CGirderModelManager::GetBearingLiveLoadReaction(IntervalIndexType intervalI
    {
       ATLASSERT(pBridge->IsInteriorPier(location.PierIdx));
 
-      GET_IFACE_NOCHECK(IReactions,pReactions);
       REACTION Rmin,Rmax;
       // get maximum vertical (Fy) reaction with corresponding rotation (Mz = Rz)
       GM_GetLiveLoadReaction(intervalIdx,llType,location.PierIdx,location.GirderKey,bat,bIncludeImpact,bIncludeLLDF,pgsTypes::fetFy,pgsTypes::fetRz,&Rmin,&Rmax,pTmin,pTmax,pMinVehIdx,pMaxVehIdx);
