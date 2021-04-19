WSDOT/PGSuper

commit d13d4c28802609305b12d323763f8b53ea796a0a

Updates implementation of CBridgeAgentImp::GetSegmentsAtPier to work for continuous piers

BridgeAgent/BridgeAgentImp.cpp
@@ -9779,7 +9779,24 @@ CSegmentKey CBridgeAgentImp::GetSegmentAtPier(PierIndexType pierIdx,const CGirde
 
 void CBridgeAgentImp::GetSegmentsAtPier(PierIndexType pierIdx, GirderIndexType gdrIdx, CSegmentKey* pBackSegmentKey, CSegmentKey* pAheadSegmentKey) const
 {
-   GET_IFACE_NOCHECK(IBridgeDescription, pIBridgeDesc);
+   GET_IFACE(IBridgeDescription, pIBridgeDesc);
+   const CPierData2* pPier = pIBridgeDesc->GetPier(pierIdx);
+   if (pPier->IsContinuous())
+   {
+      // this method is intended to return the segment keys at a boundary pier or a pier where segments connection
+      // if this method gets called for a continuous pier (segments are continuous over pier) this method should still
+      // work, so here we do the work to call the GetSegmentAtPier and then return.
+      GroupIndexType backGroupIdx, aheadGroupIdx;
+      GetGirderGroupIndex(pierIdx, &backGroupIdx, &aheadGroupIdx);
+      ATLASSERT(backGroupIdx == aheadGroupIdx);
+
+      CGirderKey girderKey(backGroupIdx, gdrIdx);
+      CSegmentKey segmentKey = GetSegmentAtPier(pierIdx, girderKey);
+      *pBackSegmentKey = segmentKey;
+      *pAheadSegmentKey = segmentKey;
+      return;
+   }
+
    CSegmentKey backSegmentKey, aheadSegmentKey;
    GroupIndexType backGroupIdx, aheadGroupIdx;
    GetGirderGroupIndex(pierIdx, &backGroupIdx, &aheadGroupIdx);

commit 9f9bdc6a3004e4e7bb5b110c5f73bc52ed2e15e8

Fixes bug introduced in commit d13d4c28 that deals with getting segments at continuous piers

BridgeAgent/BridgeAgentImp.cpp
@@ -9781,10 +9781,10 @@ void CBridgeAgentImp::GetSegmentsAtPier(PierIndexType pierIdx, GirderIndexType g
 {
    GET_IFACE(IBridgeDescription, pIBridgeDesc);
    const CPierData2* pPier = pIBridgeDesc->GetPier(pierIdx);
-   if (pPier->IsContinuous())
+   if (pPier->IsContinuous() && !pPier->IsBoundaryPier())
    {
-      // this method is intended to return the segment keys at a boundary pier or a pier where segments connection
-      // if this method gets called for a continuous pier (segments are continuous over pier) this method should still
+      // This method is intended to return the segment keys at a boundary pier or a pier where segments connect.
+      // If this method gets called for a continuous pier (segments are continuous over pier) this method should still
       // work, so here we do the work to call the GetSegmentAtPier and then return.
       GroupIndexType backGroupIdx, aheadGroupIdx;
       GetGirderGroupIndex(pierIdx, &backGroupIdx, &aheadGroupIdx);