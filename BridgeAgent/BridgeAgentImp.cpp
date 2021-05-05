WSDOT/PGSuper

commit be46203d3160b0c901d80f3f60681d861806ed94

Adds whole station labels to alignment and profile views, increases font size for labels, and fixes curbline plots

BridgeAgent/BridgeAgentImp.cpp
@@ -1825,14 +1825,14 @@ void CBridgeAgentImp::ValidateSegmentOrientation(const CSegmentKey& segmentKey) 
    segment->put_Orientation(orientation);
 }
 
-static void ComputeReasonableSurfaceStationRange(CComPtr<ICogoModel> pCogoModel, const CBridgeDescription2* pBridgeDesc, const AlignmentData2& alignmentData, CComPtr<IAlignment> alignment, 
-                                          const ProfileData2&   profileData, Float64* pStartStation, Float64* pEndStation)
+void CBridgeAgentImp::ComputeReasonableSurfaceStationRange(ICogoModel* pCogoModel, const CBridgeDescription2* pBridgeDesc, const AlignmentData2& alignmentData, IAlignment* pAlignment, const ProfileData2& profileData, Float64* pStartStation, Float64* pEndStation)
 {
    // Want a reasonable start and end station where we can put our model and retain some numerical accuracy
+   Float64 n = 10;
    Float64 bridge_length = pBridgeDesc->GetLength();
    const CPierData2* pPier = pBridgeDesc->GetPier(0);
    Float64 startStation = pPier->GetStation();
-   startStation -= bridge_length;
+   startStation -= pBridgeDesc->GetSpan(0)->GetSpanLength()/n;
 
    if ( 0 < alignmentData.HorzCurves.size() )
    {
@@ -1858,10 +1858,10 @@ static void ComputeReasonableSurfaceStationRange(CComPtr<ICogoModel> pCogoModel,
 
          CComPtr<IStation> objStation;
          Float64 offset;
-         alignment->Offset(pntTS,&objStation,&offset);
+         pAlignment->Offset(pntTS,&objStation,&offset);
 
          Float64 station;
-         objStation->get_NormalizedValue(alignment,&station);
+         objStation->get_NormalizedValue(pAlignment,&station);
          startStation = Min(startStation,station);
       }
    }
@@ -1881,14 +1881,14 @@ static void ComputeReasonableSurfaceStationRange(CComPtr<ICogoModel> pCogoModel,
       CComPtr<IStation> objStation;
       bvcPoint->get_Station(&objStation);
       Float64 station;
-      objStation->get_NormalizedValue(alignment,&station);
+      objStation->get_NormalizedValue(pAlignment,&station);
       startStation = Min(startStation,station);
    }
 
    // end station
    pPier = pBridgeDesc->GetPier(pBridgeDesc->GetPierCount()-1);
    Float64 endStation = pPier->GetStation();
-   endStation += bridge_length;
+   endStation += pBridgeDesc->GetSpan(pBridgeDesc->GetSpanCount()-1)->GetSpanLength()/n;
 
    if ( 0 < alignmentData.HorzCurves.size() )
    {
@@ -1915,10 +1915,10 @@ static void ComputeReasonableSurfaceStationRange(CComPtr<ICogoModel> pCogoModel,
 
          CComPtr<IStation> objStation;
          Float64 offset;
-         alignment->Offset(pntST,&objStation,&offset);
+         pAlignment->Offset(pntST,&objStation,&offset);
 
          Float64 station;
-         objStation->get_NormalizedValue(alignment,&station);
+         objStation->get_NormalizedValue(pAlignment,&station);
          endStation = Max(endStation,station);
       }
    }
@@ -1936,10 +1936,24 @@ static void ComputeReasonableSurfaceStationRange(CComPtr<ICogoModel> pCogoModel,
       CComPtr<IStation> objStation;
       evcPoint->get_Station(&objStation);
       Float64 station;
-      objStation->get_NormalizedValue(alignment,&station);
+      objStation->get_NormalizedValue(pAlignment,&station);
       endStation = Max(endStation,station);
    }
 
+   Float64 refStation = alignmentData.RefStation;
+   CComPtr<IPoint2d> refPoint;
+   pAlignment->LocatePoint(CComVariant(refStation), omtNormal, 0.0, CComVariant(0), &refPoint);
+   Float64 x, y;
+   refPoint->Location(&x, &y); // this is in local coordinates, we want it in global coordinates
+   x += m_DeltaX;
+   y += m_DeltaY;
+   if (!IsZero(refStation) || !IsZero(x) || !IsZero(y))
+   {
+      // we'll ignore the ref point at 0+00 (N 0, E 0)
+      // this is the default and it probably means the user didn't input the values
+      startStation = Min(startStation, refStation);
+      endStation   = Max(endStation, refStation);
+   }
    *pStartStation = startStation;
    *pEndStation = endStation;
 }

BridgeAgent/BridgeAgentImp.h
@@ -1483,6 +1483,9 @@ private:
    bool BuildGirders();
    void ValidateGirders();
 
+   // helper function for computing station range for roadway surface objects
+   void ComputeReasonableSurfaceStationRange(ICogoModel* pCogoModel, const CBridgeDescription2* pBridgeDesc, const AlignmentData2& alignmentData, IAlignment* pAlignment, const ProfileData2& profileData, Float64* pStartStation, Float64* pEndStation);
+
    // helper functions for building the bridge model
    bool LayoutPiers(const CBridgeDescription2* pBridgeDesc);
    bool LayoutGirders(const CBridgeDescription2* pBridgeDesc);

PGSuperAppPlugin/AlignmentPlanView.cpp
@@ -515,6 +515,7 @@ void CAlignmentPlanView::BuildLabelDisplayObjects()
       CreateStationLabel(label_display_list,end_station,_T("End"),    LABEL_NORMAL_TO_ALIGNMENT, TA_BASELINE | TA_RIGHT);
    }
 
+   // Start/End of station range labels
    GET_IFACE2(pBroker, IRoadway, pRoadway);
    Float64 n = 10;
    Float64 start_station, start_elevation, start_grade;
@@ -524,6 +525,18 @@ void CAlignmentPlanView::BuildLabelDisplayObjects()
    pRoadway->GetEndPoint(n, &end_station, &end_elevation, &end_grade, &pntEnd);
    CreateStationLabel(label_display_list, start_station);
    CreateStationLabel(label_display_list, end_station);
+
+   // Even station labels
+   GET_IFACE2(pBroker, IEAFDisplayUnits, pDisplayUnits);
+   Float64 station_step = (pDisplayUnits->GetUnitMode() == eafTypes::umUS ? ::ConvertToSysUnits(100.00, unitMeasure::Feet) : ::ConvertToSysUnits(100.00, unitMeasure::Meter));
+   Float64 start = ::CeilOff(start_station, station_step);
+   Float64 end = ::FloorOff(end_station, station_step);
+   Float64 station = start;
+   do
+   {
+      CreateStationLabel(label_display_list, station);
+      station += station_step;
+   } while (station < end);
 
    // Label Horizontal Curve Points
    IndexType nHC = pRoadway->GetCurveCount();
@@ -627,6 +640,7 @@ void CAlignmentPlanView::CreateStationLabel(iDisplayList* pDisplayList,Float64 s
 
    doLabel->SetTextAlign(textAlign);
    doLabel->SetBkMode(TRANSPARENT);
+   doLabel->SetPointSize(100);
 
    pDisplayList->AddDisplayObject(doLabel);
 }

PGSuperAppPlugin/AlignmentProfileView.cpp
@@ -457,10 +457,10 @@ void CAlignmentProfileView::BuildLabelDisplayObjects()
    CComPtr<iDisplayMgr> dispMgr;
    GetDisplayMgr(&dispMgr);
 
-   CComPtr<iDisplayList> label_list;
-   dispMgr->FindDisplayList(LABEL_DISPLAY_LIST,&label_list);
+   CComPtr<iDisplayList> label_display_list;
+   dispMgr->FindDisplayList(LABEL_DISPLAY_LIST,&label_display_list);
 
-   label_list->Clear();
+   label_display_list->Clear();
 
    CComPtr<IBroker> pBroker;
    EAFGetBroker(&pBroker);
@@ -473,11 +473,11 @@ void CAlignmentProfileView::BuildLabelDisplayObjects()
       GET_IFACE2(pBroker,IBridge,pBridge);
       Float64 start_station = pBridge->GetPierStation(0);
       Float64 end_station = pBridge->GetPierStation(pBridge->GetPierCount()-1);
-      CreateStationLabel(label_list,start_station,_T("Start"),TA_BASELINE | TA_LEFT);
-      CreateStationLabel(label_list,end_station,  _T("End"),  TA_BASELINE | TA_LEFT);
+      CreateStationLabel(label_display_list,start_station,_T("Start"),TA_BASELINE | TA_LEFT);
+      CreateStationLabel(label_display_list,end_station,  _T("End"),  TA_BASELINE | TA_LEFT);
    }
 
-   // Label Vertical Curves
+   // Start/End of station range labels
    GET_IFACE2(pBroker,IRoadway,pRoadway);
    Float64 n = 10;
    Float64 start_station, start_elevation, start_grade;
@@ -485,9 +485,22 @@ void CAlignmentProfileView::BuildLabelDisplayObjects()
    CComPtr<IPoint2d> pntStart, pntEnd;
    pRoadway->GetStartPoint(n, &start_station, &start_elevation, &start_grade, &pntStart);
    pRoadway->GetEndPoint(n, &end_station, &end_elevation, &end_grade, &pntEnd);
-   CreateStationLabel(label_list, start_station);
-   CreateStationLabel(label_list, end_station);
+   CreateStationLabel(label_display_list, start_station);
+   CreateStationLabel(label_display_list, end_station);
 
+   // Even station labels
+   GET_IFACE2(pBroker, IEAFDisplayUnits, pDisplayUnits);
+   Float64 station_step = (pDisplayUnits->GetUnitMode() == eafTypes::umUS ? ::ConvertToSysUnits(100.00, unitMeasure::Feet) : ::ConvertToSysUnits(100.00, unitMeasure::Meter));
+   Float64 start = ::CeilOff(start_station, station_step);
+   Float64 end = ::FloorOff(end_station, station_step);
+   Float64 station = start;
+   do
+   {
+      CreateStationLabel(label_display_list, station);
+      station += station_step;
+   } while (station < end);
+
+   // Label Vertical Curves
    IndexType nVC = pRoadway->GetVertCurveCount();
    for ( IndexType vcIdx = 0; vcIdx < nVC; vcIdx++ )
    {
@@ -508,7 +521,7 @@ void CAlignmentProfileView::BuildLabelDisplayObjects()
          Float64 elevation;
          pntPVI->get_Elevation(&elevation);
 
-         CreateStationLabel(label_list,station,elevation,_T("PVI"));
+         CreateStationLabel(label_display_list,station,elevation,_T("PVI"));
       }
       else
       {
@@ -516,13 +529,11 @@ void CAlignmentProfileView::BuildLabelDisplayObjects()
          vc->get_BVC(&pntBVC);
          CComPtr<IStation> objStation;
          pntBVC->get_Station(&objStation);
-         Float64 station;
-         objStation->get_Value(&station);
-
+         Float64 bvc_station;
+         objStation->get_Value(&bvc_station);
          Float64 elevation;
          pntBVC->get_Elevation(&elevation);
-
-         CreateStationLabel(label_list,station,elevation,_T("BVC"));
+         CreateStationLabel(label_display_list, bvc_station,elevation,_T("BVC"));
 
          //CComPtr<IProfilePoint> pntPVI;
          //vc->get_PVI(&pntPVI);
@@ -531,16 +542,41 @@ void CAlignmentProfileView::BuildLabelDisplayObjects()
          //objStation->get_Value(&station);
          //pntPVI->get_Elevation(&elevation);
 
-         //CreateStationLabel(label_list,station,elevation,_T("PVI"));
+         //CreateStationLabel(label_display_list,station,elevation,_T("PVI"));
 
          CComPtr<IProfilePoint> pntEVC;
          vc->get_EVC(&pntEVC);
          objStation.Release();
          pntEVC->get_Station(&objStation);
-         objStation->get_Value(&station);
+         Float64 evc_station;
+         objStation->get_Value(&evc_station);
          pntEVC->get_Elevation(&elevation);
 
-         CreateStationLabel(label_list,station,elevation,_T("EVC"));
+         CreateStationLabel(label_display_list, evc_station,elevation,_T("EVC"));
+
+         CComPtr<IProfilePoint> low_point;
+         vc->get_LowPoint(&low_point);
+         objStation.Release();
+         low_point->get_Station(&objStation);
+         Float64 low_point_station;
+         objStation->get_Value(&low_point_station);
+         low_point->get_Elevation(&elevation);
+         if (!IsEqual(bvc_station, low_point_station) && !IsEqual(low_point_station, evc_station))
+         {
+            CreateStationLabel(label_display_list, low_point_station, elevation, _T("LP"));
+         }
+
+         CComPtr<IProfilePoint> high_point;
+         vc->get_HighPoint(&high_point);
+         objStation.Release();
+         high_point->get_Station(&objStation);
+         Float64 high_point_station;
+         objStation->get_Value(&high_point_station);
+         high_point->get_Elevation(&elevation);
+         if (!IsEqual(bvc_station, high_point_station) && !IsEqual(high_point_station, evc_station))
+         {
+            CreateStationLabel(label_display_list, high_point_station, elevation, _T("HP"));
+         }
       }
    }
 }
@@ -617,6 +653,7 @@ void CAlignmentProfileView::CreateStationLabel(iDisplayList* pDisplayList,Float6
    doLabel->SetAngle(900);
    doLabel->SetTextAlign(textAlign);
    doLabel->SetBkMode(TRANSPARENT);
+   doLabel->SetPointSize(100);
 
    pDisplayList->AddDisplayObject(doLabel);
 }

commit 5a80ca5c2aaa772060ea197c93aa1bac0e6e4560

Fixes bug introduced in commit be46203d dealing with range of stations for display and surface model

BridgeAgent/BridgeAgentImp.cpp
@@ -1828,11 +1828,10 @@ void CBridgeAgentImp::ValidateSegmentOrientation(const CSegmentKey& segmentKey) 
 void CBridgeAgentImp::ComputeReasonableSurfaceStationRange(ICogoModel* pCogoModel, const CBridgeDescription2* pBridgeDesc, const AlignmentData2& alignmentData, IAlignment* pAlignment, const ProfileData2& profileData, Float64* pStartStation, Float64* pEndStation)
 {
    // Want a reasonable start and end station where we can put our model and retain some numerical accuracy
-   Float64 n = 10;
    Float64 bridge_length = pBridgeDesc->GetLength();
    const CPierData2* pPier = pBridgeDesc->GetPier(0);
    Float64 startStation = pPier->GetStation();
-   startStation -= pBridgeDesc->GetSpan(0)->GetSpanLength()/n;
+   startStation -= bridge_length;
 
    if ( 0 < alignmentData.HorzCurves.size() )
    {
@@ -1888,7 +1887,7 @@ void CBridgeAgentImp::ComputeReasonableSurfaceStationRange(ICogoModel* pCogoMode
    // end station
    pPier = pBridgeDesc->GetPier(pBridgeDesc->GetPierCount()-1);
    Float64 endStation = pPier->GetStation();
-   endStation += pBridgeDesc->GetSpan(pBridgeDesc->GetSpanCount()-1)->GetSpanLength()/n;
+   endStation += bridge_length;
 
    if ( 0 < alignmentData.HorzCurves.size() )
    {
@@ -1954,6 +1953,14 @@ void CBridgeAgentImp::ComputeReasonableSurfaceStationRange(ICogoModel* pCogoMode
       startStation = Min(startStation, refStation);
       endStation   = Max(endStation, refStation);
    }
+
+   if (profileData.VertCurves.size() == 0)
+   {
+      Float64 L = 100;
+      startStation = Min(startStation, profileData.Station + L);
+      endStation   = Max(endStation, profileData.Station + L);
+   }
+
    *pStartStation = startStation;
    *pEndStation = endStation;
 }