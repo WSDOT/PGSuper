3859e13de90ef55ea21d76b29a97895000ab7802
Richard Brice <37087370+RickBrice@users.noreply.github.com>
2021-10-27 15:24:57 -0700

    Mantis 1278 - Fixes problem using pgsPierLabel::CreatePierLabel in string with %s substution token.

--- PGSuperAppPlugin/BridgeDescFramingPage.cpp
+++ PGSuperAppPlugin/BridgeDescFramingPage.cpp
@@ -95,7 +95,7 @@ void CBridgeDescFramingPage::DoDataExchange(CDataExchange* pDX)
                bool bIsAbument = pierIdx == 0 || pierIdx == nPiers - 1;
                CString strMsg;
                strMsg.Format(_T("The boundary conditions for %s are invalid.\r\nPress the Edit button in the grid to update the boundary conditions."), 
-                            pgsPierLabel::CreatePierLabel(bIsAbument, pierIdx, m_DisplayStartSupportType, m_DisplayEndSupportType, m_StartingPierNumber));
+                            pgsPierLabel::CreatePierLabel(bIsAbument, pierIdx, m_DisplayStartSupportType, m_DisplayEndSupportType, m_StartingPierNumber).c_str());
                AfxMessageBox(strMsg, MB_ICONERROR | MB_OK);
                pDX->PrepareCtrl(IDC_PIER_GRID);
                pDX->Fail();
