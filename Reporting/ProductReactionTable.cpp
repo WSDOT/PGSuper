WSDOT/PGSuper

commit 369aaa0c21ee0c96e51495189c78d64fcff5718b

Fixes problems reporting diaphragm reactions

Reporting/ProductReactionTable.cpp
@@ -167,9 +167,12 @@ rptRcTable* CProductReactionTable::Build(IBroker* pBroker,const CGirderKey& gird
         (*p_table)(row,col++) << reaction.SetValue( pForces->GetReaction(erectSegmentIntervalIdx, reactionLocation, pgsTypes::pftGirder,    analysisType == pgsTypes::Simple ? pgsTypes::SimpleSpan : pgsTypes::ContinuousSpan ) );
      }
 
-     if ( reactionDecider.DoReport(diaphragmIntervalIdx) )
+     if ( reactionDecider.DoReport(diaphragmIntervalIdx) || reactionDecider.DoReport(lastCastDeckIntervalIdx))
      {
-        (*p_table)(row,col++) << reaction.SetValue( pForces->GetReaction(diaphragmIntervalIdx,     reactionLocation, pgsTypes::pftDiaphragm, analysisType == pgsTypes::Simple ? pgsTypes::SimpleSpan : pgsTypes::ContinuousSpan ) );
+        // for simple spans and expansion piers, pier diaphragm loads are applied in the diaphragm loading interval
+        // for spans made continous or integral, pier diaphragms are cast with the deck
+        // report the total diaphragm reaction by using the deck casting interval (reactions are cumulative in this function call)
+        (*p_table)(row,col++) << reaction.SetValue(pForces->GetReaction(lastCastDeckIntervalIdx, reactionLocation, pgsTypes::pftDiaphragm, analysisType == pgsTypes::Simple ? pgsTypes::SimpleSpan : pgsTypes::ContinuousSpan));
      }
      else
      {

Reporting/ReactionInterfaceAdapters.cpp
@@ -139,20 +139,7 @@ ReactionLocationContainer GetPierReactionLocations(const CGirderKey& girderKey, 
       ATLASSERT(vGirderKeys.size() == 1);
       const auto& thisGirderKey = vGirderKeys.front();
 
-      PierReactionFaceType face;
-      if (pierIdx == startPierIdx)
-      {
-         face = rftAhead;
-      }
-      else if (pierIdx == endPierIdx)
-      {
-         face = rftBack;
-      }
-      else
-      {
-         face = rftMid;
-      }
-
+      PierReactionFaceType face = rftMid;
       ReactionLocation location = MakeReactionLocation(pierIdx, nPiers, face, thisGirderKey);
       container.push_back( location );
    }

commit 0c6b89883f3b85eff6c13001053bd38c04895fd8

Fixes problem reporting diaphragm reactions for no-deck bridges introduced in commit 369aaa0c

Reporting/ProductReactionTable.cpp
@@ -167,10 +167,10 @@ rptRcTable* CProductReactionTable::Build(IBroker* pBroker,const CGirderKey& gird
         (*p_table)(row,col++) << reaction.SetValue( pForces->GetReaction(erectSegmentIntervalIdx, reactionLocation, pgsTypes::pftGirder,    analysisType == pgsTypes::Simple ? pgsTypes::SimpleSpan : pgsTypes::ContinuousSpan ) );
      }
 
-     if ( reactionDecider.DoReport(diaphragmIntervalIdx) || reactionDecider.DoReport(lastCastDeckIntervalIdx))
+     if ( reactionDecider.DoReport(diaphragmIntervalIdx) && reactionDecider.DoReport(lastCastDeckIntervalIdx))
      {
         // for simple spans and expansion piers, pier diaphragm loads are applied in the diaphragm loading interval
-        // for spans made continous or integral, pier diaphragms are cast with the deck
+        // for spans made continuous or integral, pier diaphragms are cast with the deck
         // report the total diaphragm reaction by using the deck casting interval (reactions are cumulative in this function call)
         (*p_table)(row,col++) << reaction.SetValue(pForces->GetReaction(lastCastDeckIntervalIdx, reactionLocation, pgsTypes::pftDiaphragm, analysisType == pgsTypes::Simple ? pgsTypes::SimpleSpan : pgsTypes::ContinuousSpan));
      }