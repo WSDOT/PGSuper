31d82ccb93d649b61323e00252a284590934a55c
Richard Brice <37087370+RickBrice@users.noreply.github.com>
2023-11-29 12:52:37 -0800

    Mantis 667 - More fixes related to build-release process

--- Documentation/BuildDocumentation.bat
+++ Documentation/BuildDocumentation.bat
@@ -23,17 +23,17 @@ copy PGSuper.dm PGSLibrary.dm
 REM - Copy the documentation to BridgeLink
 rmdir /S /Q %ARPDIR%\BridgeLink\Docs\PGSuper\%1\
 mkdir %ARPDIR%\BridgeLink\Docs\PGSuper\%1\
-copy %ARPDIR%\PGSuper\Documentation\PGSuper\doc\html\* %ARPDIR%\BridgeLink\Docs\PGSuper\%1\
+xcopy /s /y /d %ARPDIR%\PGSuper\Documentation\PGSuper\doc\html\* %ARPDIR%\BridgeLink\Docs\PGSuper\%1\
 copy %ARPDIR%\PGSuper\Documentation\PGSuper.dm %ARPDIR%\BridgeLink\Docs\PGSuper\%1\
 
 rmdir /S /Q %ARPDIR%\BridgeLink\Docs\PGSplice\%1\
 mkdir %ARPDIR%\BridgeLink\Docs\PGSplice\%1\
-copy %ARPDIR%\PGSuper\Documentation\PGSplice\doc\html\* %ARPDIR%\BridgeLink\Docs\PGSplice\%1\
+xcopy /s /y /d %ARPDIR%\PGSuper\Documentation\PGSplice\doc\html\* %ARPDIR%\BridgeLink\Docs\PGSplice\%1\
 copy %ARPDIR%\PGSuper\Documentation\PGSplice.dm %ARPDIR%\BridgeLink\Docs\PGSplice\%1\
 
 rmdir /S /Q %ARPDIR%\BridgeLink\Docs\PGSLibrary\%1\
 mkdir %ARPDIR%\BridgeLink\Docs\PGSLibrary\%1\
-copy %ARPDIR%\PGSuper\Documentation\PGSLibrary\doc\html\* %ARPDIR%\BridgeLink\Docs\PGSLibrary\%1\
+xcopy /s /y /d %ARPDIR%\PGSuper\Documentation\PGSLibrary\doc\html\* %ARPDIR%\BridgeLink\Docs\PGSLibrary\%1\
 copy %ARPDIR%\PGSuper\Documentation\PGSLibrary.dm %ARPDIR%\BridgeLink\Docs\PGSLibrary\%1\
 
 goto :eof
