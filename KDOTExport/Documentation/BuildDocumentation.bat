d3e8d32818d3278e12c39df2b44286471a06c140
Richard Brice <37087370+RickBrice@users.noreply.github.com>
2023-11-29 12:52:37 -0800

    Mantis 667 - More fixes related to build-release process

--- KDOTExport/Documentation/BuildDocumentation.bat
+++ KDOTExport/Documentation/BuildDocumentation.bat
@@ -17,6 +17,6 @@ echo ^</script^> >> %SEARCH_HTML%
 %ARPDIR%\BridgeLink\RegFreeCOM\x64\Release\MakeDocMap KDOT
 rmdir /S /Q %ARPDIR%\BridgeLink\Docs\KDOT\%1\
 mkdir %ARPDIR%\BridgeLink\Docs\KDOT\%1\
-copy %ARPDIR%\PGSuper\KDOTExport\Documentation\doc\html\* %ARPDIR%\BridgeLink\Docs\KDOT\%1\
+xcopy /s /y /d %ARPDIR%\PGSuper\KDOTExport\Documentation\doc\html\* %ARPDIR%\BridgeLink\Docs\KDOT\%1\
 copy %ARPDIR%\PGSuper\KDOTExport\Documentation\KDOT.dm %ARPDIR%\BridgeLink\Docs\KDOT\%1\
 
--- TxDOTAgent/Documentation/BuildDocumentation.bat
+++ TxDOTAgent/Documentation/BuildDocumentation.bat
@@ -17,7 +17,7 @@ echo ^</script^> >> %SEARCH_HTML%
 %ARPDIR%\BridgeLink\RegFreeCOM\x64\Release\MakeDocMap Toga
 rmdir /S /Q %ARPDIR%\BridgeLink\Docs\TOGA\%1\
 mkdir %ARPDIR%\BridgeLink\Docs\TOGA\%1\
-copy %ARPDIR%\PGSuper\TxDOTAgent\Documentation\TOGA\doc\html\* %ARPDIR%\BridgeLink\Docs\TOGA\%1\
+xcopy /s /y /d %ARPDIR%\PGSuper\TxDOTAgent\Documentation\TOGA\doc\html\* %ARPDIR%\BridgeLink\Docs\TOGA\%1\
 copy %ARPDIR%\PGSuper\TxDOTAgent\Documentation\TOGA\Toga.dm %ARPDIR%\BridgeLink\Docs\TOGA\%1\
 
 cd %ARPDIR%\PGSuper\TxDOTAgent\Documentation\TxCADExport
@@ -33,7 +33,7 @@ echo ^</script^> >> %SEARCH_HTML%
 %ARPDIR%\BridgeLink\RegFreeCOM\x64\Release\MakeDocMap TxCADExport
 rmdir /S /Q %ARPDIR%\BridgeLink\Docs\TxCADExport\%1\
 mkdir %ARPDIR%\BridgeLink\Docs\TxCADExport\%1\
-copy %ARPDIR%\PGSuper\TxDOTAgent\Documentation\TxCADExport\doc\html\* %ARPDIR%\BridgeLink\Docs\TxCADExport\%1\
+xcopy /s /y /d %ARPDIR%\PGSuper\TxDOTAgent\Documentation\TxCADExport\doc\html\* %ARPDIR%\BridgeLink\Docs\TxCADExport\%1\
 copy %ARPDIR%\PGSuper\TxDOTAgent\Documentation\TxCADExport\TxCADExport.dm %ARPDIR%\BridgeLink\Docs\TxCADExport\%1\
 
 cd %ARPDIR%\PGSuper\TxDOTAgent\Documentation
