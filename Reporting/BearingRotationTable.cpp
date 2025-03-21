e8fc026c48427dd559e999cb5075e0c6fb64a81b
Ron Boutilier <115045413+boutilr@users.noreply.github.com>
2025-03-21 09:41:10 -0700

    Mantis 1590 - fixes rotation tables

--- Reporting/BearingRotationTable.cpp
+++ Reporting/BearingRotationTable.cpp
@@ -700,12 +700,40 @@ rptRcTable* CBearingRotationTable::BuildBearingRotationTable(IBroker* pBroker, c
 
 
     // get poi where pier rotations occur
-    PoiList vPoi;
+    std::vector<pgsPointOfInterest> vPoi;
     std::vector<CGirderKey> vGirderKeys;
-    pBridge->GetGirderline(girderKey.girderIndex, details.startGroup, details.endGroup, &vGirderKeys);
+    GroupIndexType startGroup = (girderKey.groupIndex == ALL_GROUPS ? 0 : girderKey.groupIndex);
+    GroupIndexType endGroup = (girderKey.groupIndex == ALL_GROUPS ? pBridge->GetGirderGroupCount() - 1 : startGroup);
+    pBridge->GetGirderline(girderKey.girderIndex, startGroup, endGroup, &vGirderKeys);
     for (const auto& thisGirderKey : vGirderKeys)
     {
-        pPOI->GetPointsOfInterest(CSpanKey(ALL_SPANS, thisGirderKey.girderIndex), POI_ABUTMENT | POI_BOUNDARY_PIER | POI_INTERMEDIATE_PIER, &vPoi);
+        PierIndexType startPierIdx = pBridge->GetGirderGroupStartPier(thisGirderKey.groupIndex);
+        PierIndexType endPierIdx = pBridge->GetGirderGroupEndPier(thisGirderKey.groupIndex);
+        for (PierIndexType pierIdx = startPierIdx; pierIdx <= endPierIdx; pierIdx++)
+        {
+            if (pierIdx == startPierIdx)
+            {
+                CSegmentKey segmentKey(thisGirderKey, 0);
+                PoiList segPoi;
+                pPOI->GetPointsOfInterest(segmentKey, POI_0L | POI_ERECTED_SEGMENT, &segPoi);
+                vPoi.push_back(segPoi.front());
+            }
+            else if (pierIdx == endPierIdx)
+            {
+                SegmentIndexType nSegments = pBridge->GetSegmentCount(thisGirderKey);
+                CSegmentKey segmentKey(thisGirderKey, nSegments - 1);
+                PoiList segPoi;
+                pPOI->GetPointsOfInterest(segmentKey, POI_10L | POI_ERECTED_SEGMENT, &segPoi);
+                vPoi.push_back(segPoi.front());
+            }
+            else
+            {
+
+                Float64 Xgp;
+                VERIFY(pBridge->GetPierLocation(thisGirderKey, pierIdx, &Xgp));
+                vPoi.push_back(pPOI->ConvertGirderPathCoordinateToPoi(thisGirderKey, Xgp));
+            }
+        }
     }
     
 
