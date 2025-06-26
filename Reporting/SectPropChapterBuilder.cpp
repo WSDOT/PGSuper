0c1dbe21d79ee249480e4514dd4b2f61bb6e9e6d
rdpBridgeSight <44646153+rdpBridgeSight@users.noreply.github.com>
2025-06-26 10:28:20 -0700

    1403: Section Properties Chapter - Report Ec Girder and modular ratio, n

--- Reporting/SectPropChapterBuilder.cpp
+++ Reporting/SectPropChapterBuilder.cpp
@@ -225,14 +225,13 @@ rptChapter* CSectPropChapterBuilder::Build(const std::shared_ptr<const WBFL::Rep
          Float64 Ecdeck = pMaterial->GetDeckEc28();
         (*pPara) << _T("Deck   ") << RPT_EC << _T(" = ") << modE.SetValue( Ecdeck ) << rptNewLine;
 
-        // Report girder Ec and n if there is only one segment in this girder
+        // Report girder Ec and n only if there is one segment in this girder
         if (girderKeys.size()==1 && pBridge->GetSegmentCount(girderKeys[0].groupIndex, girderKeys[0].girderIndex)==1)
         {
-           INIT_SCALAR_PROTOTYPE(rptRcScalar, scalar, pDisplayUnits->GetScalarFormat());
-
            CSegmentKey segKey(girderKeys[0].groupIndex, girderKeys[0].girderIndex, 0);
            Float64 Ecg = pMaterial->GetSegmentEc28(segKey); 
-           (*pPara) << _T("Girder   ") << RPT_EC << _T(" = ") << modE.SetValue(Ecg) << _T(", n = (Deck ") << RPT_EC << _T(")/(Girder ") << RPT_EC << _T(") = ") << scalar.SetValue(Ecdeck/Ecg) << rptNewLine;
+           INIT_SCALAR_PROTOTYPE(rptRcScalar, scalar, pDisplayUnits->GetScalarFormat());
+           (*pPara) << _T("Girder   ") << RPT_EC << Sub(_T("28")) << _T(" = ") << modE.SetValue(Ecg) << _T(", n = (Deck ") << RPT_EC << _T(")/(Girder ") << RPT_EC << Sub(_T("28")) << _T(") = ") << scalar.SetValue(Ecdeck/Ecg) << rptNewLine;
         }
         (*pPara) << rptNewLine;
       }
