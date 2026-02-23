e508e2b48dc240e7a444f297ef2077bfa8a17a5c
boutilr <115045413+boutilr@users.noreply.github.com>
2026-02-23 09:20:48 -0800

    Revert "remove Kdt from interface"
    This reverts commit c79fb961bbb80d04488ac3019512d3e6e55acecd.

--- Include/Details.h
+++ Include/Details.h
@@ -277,6 +277,7 @@ struct SHEARCAPACITYDETAILS
    bool bIgnoreMiniumStirrupRequirementForBeta;
 
    // [OUT]
+   Float64 Kdt;
    Float64 fpops; // fpo for strand
    Float64 fpoptSegment; // fpo for segment tendons
    Float64 fpoptGirder; // fpo for girder tendons
